ce0c7c6ca200a335845e9fd1d523411a2ff6eca3
maaztausif <maaztausif48@gmail.com>
2025-09-17 18:15:13 +0200

    setup Spring Security
    -- add username and password

---
+++ src/main/java/com/maaz/SpringSecurity_Example/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.maaz.SpringSecurity_Example.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String greet(HttpServletRequest request){
+        return "hello " + request.getSession().getId();
+    }
+
+    @GetMapping("/about")
+    public String about(HttpServletRequest request){
+
+        return "About " + request.getSession().getId();
+    }
+}
